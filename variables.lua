8af11a6b10009d087d3d11e2715de42942741034
Andrew Defee <me@andrewdefee.com>
2019-11-27 14:34:38 -0500

    4.1.1 Updated versioning to match

--- variables.lua
+++ variables.lua
@@ -17,8 +17,8 @@ JokerL = JokerL or {} -- i18n
 
 Joker = {
   name = "Joker",
-  version = "4.1.0",
-  versionESO = 401000,
+  version = "4.1.1",
+  versionESO = 401010,
   author = "Lent (IGN @CallMeLent, Github @adefee)",
   color = "D66E4A",
   locale = JokerL:GetLanguage(),
